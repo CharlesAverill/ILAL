de83d56d5222aca8d5d0389e7dcb4accaf111d9b
Charles Averill <charlesaverill20@gmail.com>
2026-02-22 02:28:59 -0500

    More soundness + completeness proof + cleanup

--- theories/IL.v
+++ theories/IL.v
@@ -327,18 +327,20 @@ Proof.
     exists s'. split. assumption. assumption.
     destruct H3 as (s'' & DS1 & DS2). exists s'. split. assumption.
     econstructor; eassumption.
-  - intros s (j & Ps). revert s Ps. induction j; intros.
-      exists s. split. assumption. constructor.
-    specialize (H0 _ _ Ps). destruct H0 as (s' & Pjs' & DS).
-    specialize (IHj s' Pjs'). admit. (* Seems fake *)
+  - assert (forall n sigma, P n sigma -> exists sigma', P O sigma' /\ [C**] ok |=> (sigma', sigma)). {
+      induction n; intros.
+        exists sigma. split. assumption. constructor.
+      specialize (H0 _ _ H1). destruct H0 as (s' & Ps' & DS).
+      specialize (IHn s' Ps'). destruct IHn as (sigma' & P0 & DRS).
+      exists sigma'. split. assumption. econstructor. apply DRS. apply DS.
+    } intros s Ps. destruct Ps. eapply H1. eassumption.
   - intros s Qs. destruct (IH _ Qs) as (s' & Ps' & DS). exists s'.
     split. assumption. now constructor.
   - intros s Qs. destruct (IH _ Qs) as (s' & Ps' & DS). exists s'.
     split. assumption. now constructor.
   - intros s Qs. exists s. split. assumption. constructor.
   - intros s (Ps & Bs). exists s. split. assumption. now constructor.
-  - intros s (x' & Ps & Eq). eexists. split. eassumption.
-    unfold denote. admit. (* Wrong direction? *)
+  - intros s (x' & Ps & Eq). eexists. split. eassumption. unfold denote. admit.
   - intros s (x' & Ps). eexists. split. eassumption.
     unfold denote. admit. (* Wrong direction again *)
 Abort.
@@ -347,182 +349,122 @@ Theorem completeness :
   forall C P Q ex,
     {{P}} denote C ex {{Q}} ->
     P, [C] ex, Q.
-    Proof. 
-    induction C ; intros P Q ex DS.
-    - destruct ex.
-      -- eapply Consequence with (Q := (fun s => False)).
-        --- apply UnitEr.
-        --- intros s Hp. exact Hp.
-        --- intros s HQ. destruct (DS s HQ). inversion H. inversion H1.
-      -- eapply Consequence with (Q := P).
-        --- apply UnitOk.
-        --- intros s Hp. exact Hp.
-        --- intros s Hq. destruct (DS s Hq) as (s1 & HP & Hstep). invs Hstep. exact HP.
-   - destruct ex.
-    -- eapply Consequence with (Q := (fun s => False)).
-      --- apply AssignmentEr.
-      --- intros s Hp. exact Hp. 
-      --- intros s Hq. destruct (DS s Hq). invs H. inversion H1.
+Proof.
+  induction C ; intros P Q ex DS.
+  - destruct ex.
+    -- eapply Consequence. constructor.
+        intros s Ps. apply Ps.
+        intros s HQ. destruct (DS s HQ) as (s' & _ & H). invs H.
+    -- eapply Consequence with (Q := P). apply UnitOk.
+        intros s Ps. apply Ps.
+        intros s HQ. destruct (DS s HQ) as (s' & HP & H). invs H. assumption.
+  - destruct ex.
+    -- eapply Consequence. constructor. intros s Ps. apply Ps.
+       intros s Qs. destruct (DS s Qs) as (s' & HP & H). invs H.
     -- eapply Consequence with (Q := (fun s => exists i', P (s[i := i']) /\
-                              s i = exp (s[i := i']))).
-      --- apply AssignmentOk.
-      --- intros s Hp. exact Hp.
-      --- intros s Hq. destruct (DS s Hq). invs H. invs H1. admit. (** confused about definition for AssignmentOK **)
+                              s i = exp (s[i := i']))). constructor. easy.
+       intros s Qs. destruct (DS s Qs) as (s' & HP & H). invs H.
+       eexists. split. rewrite update_shadow. now rewrite state_upd_eq.
+       now rewrite update_eq, update_shadow, state_upd_eq.
   - destruct ex.
-  -- eapply Consequence with (Q := (fun s => False)).
-  --- apply NondetAssignmentEr.
-  --- intros s Hp. exact Hp.
-  --- intros s Hq. destruct (DS s Hq). destruct H. invs H0.
-  -- eapply Consequence with (Q := (fun s => exists x', P (s[i := x']))).
-  --- apply NondetAssignmentOk.
-  --- intros s Hp. exact Hp.
-  --- intros s Hq. destruct (DS s Hq). destruct H. invs H0. admit. 
-  - destruct ex.
-  -- set (Mid := fun s2 => exists s1, P s1 /\ [C1] ok |=> (s1, s2)).
-  set (Qsc := fun s3 => Q s3 /\ exists s1, P s1 /\ [C1] er |=> (s1, s3)).
-  set (Qnm := fun s3 => Q s3 /\ exists s2, Mid s2 /\ [C2] er |=> (s2, s3)).
-  
-  assert (DS_C1_er : {{P}} denote C1 er {{Qsc}}).
-  {
-    intros s3 HQsc.
-    destruct HQsc. destruct H0. destruct H0. 
-    exists x. split; assumption.
-  } 
-  
-  pose proof (IHC1 P Qsc er DS_C1_er) as D_C1_er.
-  pose proof (SeqShortCircuit P Qsc C1 C2 D_C1_er) as D_sc.
+    -- eapply Consequence. constructor. intros s Ps. apply Ps.
+       intros s Qs. destruct (DS s Qs) as (s' & HP & H). invs H.
+    -- eapply Consequence with (Q := (fun s => exists x', P (s[i := x']))).
+         constructor. easy.
+       intros s Qs. destruct (DS s Qs) as (s' & HP & H). invs H.
+       eexists. rewrite update_shadow. now rewrite state_upd_eq.
+ - destruct ex.
+   -- set (Mid := fun s2 => exists s1, P s1 /\ [C1] ok |=> (s1, s2)).
+      set (Qsc := fun s3 => Q s3 /\ exists s1, P s1 /\ [C1] er |=> (s1, s3)).
+      set (Qnm := fun s3 => Q s3 /\ exists s2, Mid s2 /\ [C2] er |=> (s2, s3)).
 
-  assert (DS_C1_ok : {{P}} denote C1 ok {{Mid}}).
-  {
-    intros s2 Hmid.
-    destruct Hmid. destruct H.
-    exists x. split; assumption. 
-  }  
-   
-  pose proof (IHC1 P Mid ok DS_C1_ok) as D_C1_ok.
-  
-  assert (DS_C2_er : {{Mid}} denote C2 er {{Qnm}}).
-  {
-    intros s3 HQnm.
-    destruct HQnm. destruct H0. destruct H0.
-    exists x. split; assumption.
-  }
-  
-  pose proof (IHC2 Mid Qnm er DS_C2_er) as D_C2_er.
-  
-  pose proof (SeqNormal P Mid Qnm C1 C2 er D_C1_ok D_C2_er) as D_nm.
-  
-  pose proof (Disjunction P Qsc P Qnm <{ C1;; C2 }> er D_sc D_nm) as D_or.
-  
-  eapply Consequence with
-  (P  := fun s => P s \/ P s)
-  (Q  := fun s => Qsc s \/ Qnm s).
-  
-  --- exact D_or.
-  --- intros s [Hp|Hp]; assumption.
-  --- intros s3 HQ.   destruct (DS s3 HQ) as (s1 & HP & Hseq). inversion Hseq; subst.
-  ---- left. split ; [exact HQ|]. exists s1. split; [exact HP | assumption]. 
-  ---- right. split; [exact HQ|]. destruct H2 as (s2 & Hc1ok & Hc2er). exists s2. split.  exists s1. split; assumption. exact Hc2er.
-  
-  -- set (Mid := fun s2 => exists s1, P s1 /\ [C1] ok |=> (s1, s2)).
-    set (Qnm := fun s3 => Q s3 /\ exists s2, Mid s2 /\ [C2] ok |=> (s2, s3)).
-  
-   assert (DS_C1_ok : {{P}} denote C1 ok {{Mid}}).
-  {
-    intros s2 Hmid.
-    destruct Hmid. destruct H.
-    exists x. split; assumption. 
-  }  
-  
-  assert (DS_C2_ok : {{Mid}} denote C2 ok {{Qnm}}).
-  {
-  intros s2 Hmid. 
-  destruct Hmid. destruct H0. destruct H0.
-  exists x. split. assumption.
-  auto.
-  }
-  
-  specialize (IHC1 P Mid ok DS_C1_ok).
-  specialize (IHC2 Mid Qnm ok DS_C2_ok).
-  pose proof (SeqNormal P Mid Qnm C1 C2 ok IHC1 IHC2) as D_nm.
-  eapply Consequence with (Q := fun s => Qnm s).
-  --- exact D_nm. 
-  --- intro. trivial.
-  --- intros s3 HQ. destruct (DS s3 HQ). destruct H. unfold Qnm. split. auto. unfold Mid. invs H0. destruct H4. exists x0. split.
-  ---- destruct H0. exists x. split; assumption.
-  ---- destruct H0. assumption.
+      assert (DS_C1_er : {{P}} denote C1 er {{Qsc}}).
+        intros s3 (HQsc & s & Ps & DS'). now exists s.
+
+      pose proof (IHC1 P Qsc er DS_C1_er) as D_C1_er.
+      pose proof (SeqShortCircuit P Qsc C1 C2 D_C1_er) as D_sc.
+
+      assert (DS_C1_ok : {{P}} denote C1 ok {{Mid}}).
+       intros s2 (s & Ps & DS'). now exists s.
+
+      pose proof (IHC1 P Mid ok DS_C1_ok) as D_C1_ok.
+
+      assert (DS_C2_er : {{Mid}} denote C2 er {{Qnm}}).
+        intros s3 (HQnm & s & Ps & DS'). now exists s.
+
+      pose proof (IHC2 Mid Qnm er DS_C2_er) as D_C2_er.
+      pose proof (SeqNormal P Mid Qnm C1 C2 er D_C1_ok D_C2_er) as D_nm.
+      pose proof (Disjunction P Qsc P Qnm <{ C1;; C2 }> er D_sc D_nm) as D_or.
+
+      eapply Consequence with
+       (P  := fun s => P s \/ P s)
+       (Q  := fun s => Qsc s \/ Qnm s).
+      exact D_or. now intros s [].
+      intros s HQ. destruct (DS s HQ) as (s1 & HP & Hseq). invs Hseq.
+     + left. split. assumption. exists s1. now split.
+     + right. split. assumption. destruct H2 as (s2 & Hc1ok & Hc2er).
+       exists s2. split. exists s1. now split. assumption.
+   -- set (Mid := fun s2 => exists s1, P s1 /\ [C1] ok |=> (s1, s2)).
+      set (Qnm := fun s3 => Q s3 /\ exists s2, Mid s2 /\ [C2] ok |=> (s2, s3)).
+
+      assert (DS_C1_ok : {{P}} denote C1 ok {{Mid}}).
+        intros s2 (s & Ps & DS'). now exists s.
+
+      assert (DS_C2_ok : {{Mid}} denote C2 ok {{Qnm}}).
+        intros s2 (Qs2 & s & Ps & DS'). now exists s.
+
+      specialize (IHC1 P Mid ok DS_C1_ok).
+      specialize (IHC2 Mid Qnm ok DS_C2_ok).
+      pose proof (SeqNormal P Mid Qnm C1 C2 ok IHC1 IHC2) as D_nm.
+      eapply Consequence with (Q := fun s => Qnm s).
+        apply D_nm. now intro.
+      intros s HQ. destruct (DS s HQ) as (s1 & HP & Hseq). split. assumption.
+      invs Hseq. destruct H2 as (s2 & DS1 & DS2). exists s2. split.
+      + exists s1. now split.
+      + assumption.
   - set (Mid1 := fun s2 => exists s1, P s1 /\ [C1] ex |=> (s1, s2)).
-  set (Mid2 := fun s2 => exists s1, P s1 /\ [C2] ex |=> (s1, s2)).
-  set (Or := fun s2 => Mid1 s2 \/ Mid2 s2).
-  
-  eapply Consequence with (Q := Or).
-  -- assert (DS_C1 : {{P}} denote <{ C1 }> ex {{Mid1}}). {
-    intros s H. destruct H. destruct H. unfold ds_post. exists x. split.
-    - assumption.
-    - unfold denote. exact H0.
-  }
-  assert (DS_C2 : {{P}} denote <{ C2 }> ex {{Mid2}}). {
-    intros s H. destruct H. destruct H. unfold ds_post. exists x. split.
-    - assumption.
-    - unfold denote. exact H0.
-  }
-  
-  specialize (IHC1 P Mid1 ex DS_C1).
-  specialize (IHC2 P Mid2 ex DS_C2).
-  assert (A1 : P, [C1 <+> C2] ex, Mid1). {
-    apply ChoiceLeft. assumption.
-  }
-  assert (A2 : P, [C1 <+> C2] ex, Mid2). {
-    apply ChoiceRight. assumption.
-  }
-  apply Disjunction.
-  --- exact A1.
-  --- exact A2.
-  -- intros s Hp. destruct Hp; assumption.
-  -- intros s Hq. destruct (DS s Hq). destruct H. invs H0; unfold Or.
-  --- left. unfold Mid1. exists x. split; assumption.
-  --- right. unfold Mid2. exists x. split; assumption.
-  - set (p := fun n => ((ds_post (denote (nRepeat C n) ex) P))). 
-  eapply Consequence with (Q := fun s => exists n, (p n) s). 
-  -- destruct ex. 
-  --- admit.
-  --- eapply BackwardsVariant with (P := p).
-  intro n.
-  assert (DS_C : {{p n}} denote C ok {{p (S n)}}).
-  {
-  intros x Hp.
-  invs Hp. destruct H. invs H0. invs H4. destruct H0. destruct n.
-  - simpl in H0. invs H0. unfold p. simpl. unfold ds_post. exists x1. split. 
-  -- exists x1. split. assumption. simpl. constructor.
-  -- unfold denote. assumption.
-  - simpl in H0. unfold p. simpl. unfold ds_post. exists x1. split.
-  -- exists x0. split. assumption. simpl. exact H0.
-  -- simpl. exact H1.
-  } 
-  
-  specialize (IHC (p n) (p (S n)) ok DS_C).
-  assumption.
-  -- intros s H. destruct H. destruct H. invs H0. assumption.
-  -- intros s H.
-  unfold under_approximate in DS. specialize (DS s H). invs DS. invs H0.
-  unfold p. apply repeat_is_star. unfold ds_post. exists x ; auto.
- 
+    set (Mid2 := fun s2 => exists s1, P s1 /\ [C2] ex |=> (s1, s2)).
+    set (Or := fun s2 => Mid1 s2 \/ Mid2 s2).
+
+    eapply Consequence with (Q := Or).
+    -- assert (DS_C1 : {{P}} denote <{ C1 }> ex {{Mid1}}).
+         intros s (s' & Ps' & DS'). now exists s'.
+       assert (DS_C2 : {{P}} denote <{ C2 }> ex {{Mid2}}).
+         intros s (s' & Ps' & DS'). now exists s'.
+
+       specialize (IHC1 P Mid1 ex DS_C1).
+       specialize (IHC2 P Mid2 ex DS_C2).
+       assert (A1 : P, [C1 <+> C2] ex, Mid1) by now constructor.
+       assert (A2 : P, [C1 <+> C2] ex, Mid2) by now constructor.
+       constructor; eassumption.
+    -- now intros s [].
+    -- intros s Hq. destruct (DS s Hq) as (s' & Ps' & DS'). invs DS'.
+       + left. now exists s'.
+       + right. now exists s'.
+ - set (p := fun n => ((ds_post (denote (nRepeat C n) ex) P))). 
+   eapply Consequence with (Q := fun s => exists n, (p n) s).
+   -- destruct ex.
+      + admit.
+      + eapply BackwardsVariant with (P := p). intros.
+        assert (DS_C: {{p n}} denote C ok {{p (S n)}}). {
+          intros s Psn. invs Psn. destruct H as (Px & DS'). invs DS'.
+          destruct H2 as (s2 & DS1 & DS2). destruct n.
+            simpl in DS1. invs DS1. unfold p. simpl. exists s2.
+            split. exists s2. split. assumption. constructor. assumption.
+          unfold p. simpl in *. exists s2. split.
+            exists x. split; assumption.
+          assumption. } apply (IHC (p n) (p (S n)) ok DS_C).
+   -- intros s (? & ? & ?). invs H0. assumption.
+   -- intros s Qs. specialize (DS s Qs). invs DS. destruct H as (Px & DRS).
+      unfold p. apply repeat_is_star. now exists x.
   - destruct ex.
-  -- eapply Consequence with (Q := P).
-  --- apply ErrorEr.
-  --- intros s Hp. exact Hp.
-  ---  intros s Hq. destruct (DS s Hq). destruct H. invs H0. exact H.
-  -- eapply Consequence with (Q := (fun s => False)). 
-  --- apply ErrorOk.
-  --- intros s Hp. exact Hp.
-  --- intros s Hq. destruct (DS s Hq). destruct H. inversion H0.
+    -- eapply Consequence. apply ErrorEr. intros s Ps. apply Ps.
+       intros s Qs. destruct (DS s Qs) as (s' & Ps' & DS'). now invs DS'.
+    -- eapply Consequence with (Q := (fun _ => False)). apply ErrorOk. intros s Ps. apply Ps.
+       intros s Qs. destruct (DS s Qs) as (s' & Ps' & DS'). invs DS'.
   - destruct ex.
-  -- eapply Consequence with (Q := (fun s => False)).
-  --- apply AssumeEr.
-  --- intros s Hp. exact Hp.
-  --- intros s Hq. destruct (DS s Hq). destruct H. inversion H0.
-  -- eapply Consequence with (Q := (fun s => P s /\ p s)). 
-  --- apply AssumeOk.
-  --- intros s Hp. exact Hp.
-  --- intros s Hq. destruct (DS s Hq). destruct H. invs H0. auto.
-Abort.
+    -- eapply Consequence with (Q := (fun s => False)). apply AssumeEr. intros s Ps. apply Ps.
+       intros s Qs. destruct (DS s Qs) as (s' & Ps' & DS'). invs DS'.
+    -- eapply Consequence with (Q := (fun s => P s /\ p s)). apply AssumeOk. intros s Ps. apply Ps.
+       intros s Qs. destruct (DS s Qs) as (s' & Ps' & DS'). now invs DS'.
+Admitted.
